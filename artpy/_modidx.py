50838b1caf85d306ec7664f913d9f7cf467258d2
Meng Lee <meng.lee@smartnews.com>
2022-10-23 15:16:51 +0900

    feat: better support for slack rich text

--- artpy/_modidx.py
+++ artpy/_modidx.py
@@ -5,4 +5,11 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://leemengtaiwan.github.io',
                 'git_url': 'https://github.com/leemengtaiwan/artpy',
                 'lib_path': 'artpy'},
-  'syms': {'artpy.core': {'artpy.core.foo': ('core.html#foo', 'artpy/core.py')}}}
+  'syms': { 'artpy.core': {'artpy.core.foo': ('core.html#foo', 'artpy/core.py')},
+            'artpy.ui.slack': { 'artpy.ui.slack.get_image_from_file': ('ui.slack.html#get_image_from_file', 'artpy/ui/slack.py'),
+                                'artpy.ui.slack.get_key_val': ('ui.slack.html#get_key_val', 'artpy/ui/slack.py'),
+                                'artpy.ui.slack.parse_app_mention_outer_event': ( 'ui.slack.html#parse_app_mention_outer_event',
+                                                                                  'artpy/ui/slack.py'),
+                                'artpy.ui.slack.parse_raw_text': ('ui.slack.html#parse_raw_text', 'artpy/ui/slack.py'),
+                                'artpy.ui.slack.parse_rich_text': ('ui.slack.html#parse_rich_text', 'artpy/ui/slack.py'),
+                                'artpy.ui.slack.parse_rich_text_list': ('ui.slack.html#parse_rich_text_list', 'artpy/ui/slack.py')}}}
